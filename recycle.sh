34b057ef10eef6f044c8b38ebe2024a57731a603
Matthew Mattox <mmattox@support.tools>
2022-03-22 01:42:18 -0500

    Working

--- recycle.sh
+++ recycle.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for pod in `kubectl -n uptime-kuma get pods -o go-template='{{ range  $item := .items }}{{ range .status.conditions }}{{ if (or (and (eq .type "PodScheduled") (eq .status "False")) (and (eq .type "Ready") (eq .status "False"))) }}{{ $item.metadata.name}} {{ end }}{{ end }}{{ end }}'`
+echo "Looking for failed or crashing pods"
+for pod in `kubectl -n uptime-kuma get pods -l app=uptime-kuma -o go-template='{{ range  $item := .items }}{{ range .status.conditions }}{{ if (or (and (eq .type "PodScheduled") (eq .status "False")) (and (eq .type "Ready") (eq .status "False"))) }}{{ $item.metadata.name}} {{ end }}{{ end }}{{ end }}'`
 do
   kubectl -n uptime-kuma delete pod $pod
 done
+echo "Done"
