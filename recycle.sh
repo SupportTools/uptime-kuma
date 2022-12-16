1458ee9e008c834adffa702fad581b31ba19d949
Matthew Mattox <mmattox@support.tools>
2022-12-16 13:29:39 -0600

    Moving chart to all templates

--- recycle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Looking for failed or crashing pods"
-for pod in `kubectl get pods -l app=uptime-kuma -o go-template='{{ range  $item := .items }}{{ range .status.conditions }}{{ if (or (and (eq .type "PodScheduled") (eq .status "False")) (and (eq .type "Ready") (eq .status "False"))) }}{{ $item.metadata.name}} {{ end }}{{ end }}{{ end }}'`
-do
-  kubectl delete pod $pod
-done
-echo "Done"
